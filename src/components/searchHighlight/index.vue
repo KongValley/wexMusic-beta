62536aa2a5ce451ac8126f9045b2bf4dbb0ebee4
KongValley <kzp13968320676@163.com>
2019-03-08 13:39:15 +0800

    :sparkles: Add 添加搜索词高亮组件

---
+++ src/components/searchHighlight/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span v-for="item in searchArr" :key="item" :class="{'is-high-light': item === keyword }">{{item}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search-high-light",
+  props: {
+    keyword: {
+      type: String,
+      required: true
+    },
+    str: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    _getHighlightStrArr(str, key) {
+      return str.replace(new RegExp(`${key}`, 'g'), `%%${key}%%`).split('%%')
+    }
+  },
+  computed: {
+    searchArr() {
+      return this._getHighlightStrArr(this.str, this.keyword)
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.is-high-light {
+  color: #6188B5;
+}
+</style>
--- src/pages/search-detail/search-detail.vue
+++ src/pages/search-detail/search-detail.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="p-sd">
     <div class="header">
-      <div class="header-text">"{{keyword}}" 的搜索结果</div>
+      <div class="header-text">“{{keyword}}” 的搜索结果</div>
     </div>
     <div class="center">
       <i-tabs :current="currentPage" @change="handleChangeScroll" custom-class="center-tabs" :border="false" color="#fff">
         <i-tab title="单曲" :title-style="titleStyle">
-        </i-tab>
-        <i-tab title="歌单" :title-style="titleStyle">
+          <div class="list-cnt"  v-if="currentType === typeList[0]">
+            <div class="list-item">
+              <div class="item-title">
+                <div class="title-header">
+                  <c-highlight :keyword="keyword" :str="'海阔天空6667777'"></c-highlight>
+                </div>
+                <div class="title-desc">
+                  <c-highlight :keyword="keyword" :str="'Mo-哈哈哈'"></c-highlight>
+                </div>
+              </div>
+              <div class="item-action">
 
+              </div>
+            </div>
+          </div>
         </i-tab>
         <i-tab title="专辑" :title-style="titleStyle">
-
+          <div class="list-cnt"  v-if="currentType === typeList[1]">
+            <div class="list-item"></div>
+          </div>
+        </i-tab>
+        <i-tab title="歌手" :title-style="titleStyle">
+          <div class="list-cnt"  v-if="currentType === typeList[2]">
+            <div class="list-item"></div>
+          </div>
+        </i-tab>
+        <i-tab title="歌单" :title-style="titleStyle">
+          <div class="list-cnt"  v-if="currentType === typeList[3]">
+            <div class="list-item"></div>
+          </div>
+        </i-tab>
+        <i-tab title="电台" :title-style="titleStyle">
+          <div class="list-cnt" v-if="currentType === typeList[4]">
+            <div class="list-item"></div>
+          </div>
         </i-tab>
       </i-tabs>
       <div class="tab-footer"></div>
@@ -20,13 +49,47 @@
 </template>
 
 <script>
+import CHighlight from '_c/searchHighlight'
+import { getSearchDataAPI } from '_a/search'
+const typeList = [
+  1, // 单曲
+  10, // 专辑
+  100, // 歌手
+  1000, // 歌单
+  1009 // 电台
+]
 export default {
   name: "search-detail",
+  components: {
+    CHighlight
+  },
   data() {
     return {
       keyword: "",
-      currentPage: 1,
-      titleStyle: "background:#DF4337;color:#fff;"
+      currentPage: 0,
+      titleStyle: "background:#DF4337;color:#fff;",
+      currentType: 1, // 1:单曲 10:专辑 100:歌手 1000:歌单 1009: 电台
+      albumsResult: {
+        offset: 0,
+        list: []
+      },
+      songsResult: {
+        offset: 0,
+        list: []
+      },
+      djsResult: {
+        offset: 0,
+        list: []
+      },
+      singersResult: {
+        offset: 0,
+        list: []
+      },
+      playlistResult: {
+        offset: 0,
+        list: []
+      },
+      typeList: typeList
     };
   },
   methods: {
@@ -37,10 +100,25 @@ export default {
     },
     handleChangeScroll({detail}) {
       this.currentPage = detail.index
+      this.currentType = typeList[detail.index]
+    },
+    async fetchSearchResult() {
+      try {
+        // 1:单曲 10:专辑 100:歌手 1000:歌单 1002: 用户 1004: MV 1006: 歌词 1009: 电台
+        const params = {
+          keywords: this.keyword,
+          type: this.currentType
+        }
+        const a = await getSearchDataAPI(params)
+      } catch (e) {
+        console.warn(e)
+      }
     }
   },
   onLoad(option) {
     this.keyword = option.search
+    // test
+    this.keyword = '海阔天空'
   }
 }
 </script>
@@ -62,16 +140,27 @@ export default {
   }
   .center {
     /deep/ .center-tabs {
-      .van-tabs--line.van-tabs__wrap {
+      /*隐藏滚动条*/
+      ::-webkit-scrollbar {
+        width: 0;
+        height: 0;
+        color: transparent;
+
       }
       .van-tab {
         box-sizing: border-box;
         padding:0;
       }
-    }
-    .tab-footer {
-      background-color: #DF4337;
-      height: 5px;
+
+      .list-cnt {
+        width: 100%;
+        height: 100%;
+        font-size: 14px;
+      }
+
+      .van-tabs__line.van-tabs__line {
+        bottom: 8px;
+      }
     }
   }
 }
